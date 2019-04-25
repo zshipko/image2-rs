1f4e6c999a4ded76909775920f68085cfb388d96
Zach Shipko <zachshipko@gmail.com>
2019-04-25 09:50:47 -0700

    Fix rawvideo argument

--- src/io/ffmpeg.rs
+++ src/io/ffmpeg.rs
@@ -49,7 +49,7 @@ impl FFmpeg {
     }
 
     pub fn input_pipe(&mut self) -> &mut FFmpeg {
-        self.arg("-rawvideo").input_file("-")
+        self.arg("-frawvideo").input_file("-")
     }
 
     pub fn fps(&mut self, i: usize) -> &mut FFmpeg {
@@ -61,7 +61,7 @@ impl FFmpeg {
     }
 
     pub fn output_pipe(&mut self) -> &mut FFmpeg {
-        self.arg("-rawvideo").output_file("-")
+        self.arg("-frawvideo").output_file("-")
     }
 
     pub fn pix_fmt(&mut self, fmt: &str) -> &mut FFmpeg {
