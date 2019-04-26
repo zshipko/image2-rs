ee127f82c4c873643b25aa7a0ad2bccc85cf786c
Zach Shipko <zachshipko@gmail.com>
2019-04-26 08:49:23 -0700

    update

--- src/image.rs
+++ src/image.rs
@@ -461,13 +461,12 @@ pub trait Image<T: Type, C: Color>: Sized + Sync + Send {
         });
     }
 
-    fn gamma_multiply(&mut self, gamma: f64, pixel: &crate::PixelVec<f64>) {
+    fn gamma_multiply<'a, P: Pixel<'a, f64, C>>(&mut self, gamma: f64, pixel: &P) {
         let channels = C::channels();
+        let pixel = pixel.to_vec();
         self.for_each(|(_, _), px| {
             for i in 0..channels {
-                px[i] = T::from_float(T::clamp(
-                    T::to_float(&px[i]).powf(1.0 / gamma) * pixel.as_ref()[i],
-                ));
+                px[i] = T::from_float(T::clamp(T::to_float(&px[i]).powf(1.0 / gamma) * pixel[i]));
             }
         });
     }
