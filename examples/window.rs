3845d94df7cdfd003343b2474d0e64d61f57ecf5
Zach Shipko <zachshipko@gmail.com>
2020-10-05 20:30:06 -0700

    update

---
+++ examples/window.rs
@@ -0,0 +1,36 @@
+use image2::window::*;
+use image2::*;
+
+fn main() {
+    let args = std::env::args().skip(1).collect::<Vec<_>>();
+    let arg = if !args.is_empty() {
+        args[0].as_str()
+    } else {
+        "images/A.exr"
+    };
+
+    let image = Image::<f32, Rgb>::open(arg).unwrap();
+
+    let evloop = EventLoop::new();
+
+    let mut window = Window::new(&evloop, image, WindowBuilder::new().with_title(arg)).unwrap();
+
+    evloop.run(move |event, _, cf| {
+        *cf = ControlFlow::Poll;
+        match event {
+            Event::LoopDestroyed => return,
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => {
+                    *cf = ControlFlow::Exit;
+                }
+                WindowEvent::CursorMoved { position, .. } => {
+                    println!("Size: {}x{}", window.size.width, window.size.height);
+                    println!("{}, {}", position.x, position.y);
+                }
+                _ => (),
+            },
+            Event::RedrawRequested(_) => window.draw().unwrap(),
+            _ => (),
+        }
+    });
+}
