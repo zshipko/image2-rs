28c10aadca0b69280caa8c116098e40eac2170cf
Zach Shipko <zachshipko@gmail.com>
2020-12-13 13:25:27 -0800

    fix docs

--- build.rs
+++ build.rs
@@ -1,10 +1,7 @@
 fn main() {
     #[cfg(feature = "oiio")]
-    {
-        cpp_build::Config::new().build("src/io/oiio.rs");
+    cpp_build::Config::new().build("src/io/oiio.rs");
 
-        if cfg!(not(feature = "docs-rs")) {
-            println!("cargo:rustc-link-lib=OpenImageIO");
-        }
-    }
+    #[cfg(feature = "oiio")]
+    println!("cargo:rustc-link-lib=OpenImageIO");
 }
