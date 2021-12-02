20c54120cf09189069b9d0d9d029f9286d6e8493
zach <zachshipko@gmail.com>
2021-12-02 07:15:58 -0800

    Use c++14

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 fn main() {
     #[cfg(all(feature = "oiio", not(feature = "docs-rs")))]
-    cpp_build::Config::new().build("src/io/oiio.rs");
+    cpp_build::Config::new()
+        .flag("-std=c++14")
+        .build("src/io/oiio.rs");
 
     #[cfg(all(feature = "oiio", not(feature = "docs-rs")))]
     println!("cargo:rustc-link-lib=OpenImageIO");
