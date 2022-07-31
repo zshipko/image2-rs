c0d761288c546b2eec36fd81d7b1c58169d266f9
zach <zachshipko@gmail.com>
2022-07-30 17:04:48 -0700

    Updates for macos

--- build.rs
+++ build.rs
@@ -1,9 +1,29 @@
 fn main() {
     #[cfg(all(feature = "oiio", not(feature = "docs-rs")))]
     {
-        cpp_build::Config::new()
+        let mut pkg = std::process::Command::new("pkg-config");
+        pkg.arg("--cflags")
+            .arg("--libs")
+            .arg("--silence-errors")
+            .arg("OpenImageIO");
+
+        if cfg!(target_os = "macos") {
+            pkg.arg("Imath");
+        }
+
+        let pkg = pkg.output().unwrap();
+        let flags = String::from_utf8(pkg.stdout).unwrap();
+        let flags = flags.split(" ");
+        let mut config = cpp_build::Config::new();
+        config
             .flag("-std=c++14")
-            .build("src/io/oiio.rs");
+            .include("/opt/homebrew/Cellar/openimageio/2.3.17.0/include");
+
+        for flag in flags {
+            config.flag(flag);
+        }
+
+        config.build("src/io/oiio.rs");
 
         println!("cargo:rustc-link-lib=OpenImageIO");
         println!("cargo:rustc-link-lib=OpenImageIO_Util");
