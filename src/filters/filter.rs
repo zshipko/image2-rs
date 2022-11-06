e69057bc2707d7588faf80cc9ee1c1391adde7d1
zach <zachshipko@gmail.com>
2022-11-06 09:33:03 -0800

    use double for exposure filter

--- src/filters/filter.rs
+++ src/filters/filter.rs
@@ -49,17 +49,17 @@ impl<T: Type, C: Color, U: Type, D: Color> Filter<T, C, U, D> for Brightness {
 }
 
 #[derive(Debug)]
-struct Exposure(i32);
+struct Exposure(f64);
 
 /// Adjust image exposure, the argument is the number of stops to increase or decrease exposure by
-pub fn exposure<T: Type, C: Color, U: Type, D: Color>(stops: i32) -> impl Filter<T, C, U, D> {
+pub fn exposure<T: Type, C: Color, U: Type, D: Color>(stops: f64) -> impl Filter<T, C, U, D> {
     Exposure(stops)
 }
 
 impl<T: Type, C: Color, U: Type, D: Color> Filter<T, C, U, D> for Exposure {
     fn compute_at(&self, pt: Point, input: &Input<T, C>, data: &mut DataMut<U, D>) {
         let mut px = input.get_pixel(pt, None);
-        px *= 2f64.powi(self.0);
+        px *= 2f64.powf(self.0);
         px.convert_to_data(data);
     }
 }
